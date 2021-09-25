47fcfa170b281b152b8ea0623bd1ce4f28fb17d9
anjalinair <anjali.nair@airfi.aero>
2021-09-25 19:33:50 +0530

    adding humidity info

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
 
-        title: "About me",
+        title: "About",
         name: "Anjali K Nair"
 
     });
