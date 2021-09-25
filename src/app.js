5bc28164c05b49be70fb15d50dc5ef9ac7726783
anjalinair <anjali.nair@airfi.aero>
2021-09-25 18:05:25 +0530

    heroku port change, url change

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geoCode = require('./utils/geoCode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000;
 
 //define paths for express config
 const directoryPath = path.join(__dirname, '../public');
@@ -108,7 +109,7 @@ app.get('*', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port: '+port)
 })
 
